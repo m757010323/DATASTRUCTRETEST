a6a4fc6fc6f50030edd3f06fbed11934b52f6f05
habbyppy <35439037+habbyppy@users.noreply.github.com>
2019-06-23 20:37:08 +0800

    a Commit demo

--- GitTest/src/cn/mo/GitTestdemo.java
+++ GitTest/src/cn/mo/GitTestdemo.java
@@ -4,6 +4,7 @@ public class GitTestdemo {
     public static void main(String[] args) {
         System.out.println("HelloWorld");
         System.out.println("HelloGit");
+        System.out.println("modelAndView");
 
     }
 
