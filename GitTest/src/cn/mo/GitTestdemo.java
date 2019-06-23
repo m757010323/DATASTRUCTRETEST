7673813023a8a14c9aaf97d664709001a1383062
habbyppy <35439037+habbyppy@users.noreply.github.com>
2019-06-23 19:45:07 +0800

    a Commit demo

---
+++ GitTest/src/cn/mo/GitTestdemo.java
@@ -0,0 +1,10 @@
+package cn.mo;
+
+public class GitTestdemo {
+    public static void main(String[] args) {
+        System.out.println("HelloWorld");
+        System.out.println("HelloGit");
+
+    }
+
+}
